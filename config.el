2638fd6491bc1b0792d84f64f2df5a92726386c6
Oskar Kvist <oskar.kvist@gmail.com>
2020-03-26 12:11:56 +0100

    Just commiting WIP for safekeeping

--- config.el
+++ config.el
@@ -2,6 +2,59 @@
 (require 'esexp)
 (require 'utils)
 
+;; (defun $lisp-indent-function (indent-point state)
+;;   "Override `lisp-indent-function' to properly handle plists. See the original function fo full description"
+;;   (let ((normal-indent (current-column))
+;;         (orig-point (point)))
+;;     (goto-char (1+ (elt state 1)))
+;;     (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
+;;     (cond
+;;      ;; car of form doesn't seem to be a symbol, or is a keyword
+;;      ((and (elt state 2)
+;;            (or (not (looking-at "\\sw\\|\\s_"))
+;;                (looking-at ":")))
+;;       (if (not (> (save-excursion (forward-line 1) (point))
+;;                   calculate-lisp-indent-last-sexp))
+;;           (progn (goto-char calculate-lisp-indent-last-sexp)
+;;                  (beginning-of-line)
+;;                  (parse-partial-sexp (point)
+;;                                      calculate-lisp-indent-last-sexp 0 t)))
+;;       ;; Indent under the list or under the first sexp on the same
+;;       ;; line as calculate-lisp-indent-last-sexp.  Note that first
+;;       ;; thing on that line has to be complete sexp since we are
+;;       ;; inside the innermost containing sexp.
+;;       (backward-prefix-chars)
+;;       (current-column))
+;;      ((and (save-excursion
+;;              (goto-char indent-point)
+;;              (skip-syntax-forward " ")
+;;              (not (looking-at ":")))
+;;            (save-excursion
+;;              (goto-char orig-point)
+;;              (looking-at ":")))
+;;       (save-excursion
+;;         (goto-char (+ 2 (elt state 1)))
+;;         (current-column)))
+;;      (t
+;;       (let ((function (buffer-substring (point)
+;;                                         (progn (forward-sexp 1) (point))))
+;;             method)
+;;         (setq method (or (function-get (intern-soft function)
+;;                                        'lisp-indent-function)
+;;                          (get (intern-soft function) 'lisp-indent-hook)))
+;;         (cond ((or (eq method 'defun)
+;;                    (and (null method)
+;;                         (> (length function) 3)
+;;                         (string-match "\\`def" function)))
+;;                (lisp-indent-defform state indent-point))
+;;               ((integerp method)
+;;                (lisp-indent-specform method state
+;;                                      indent-point normal-indent))
+;;               (method
+;;                (funcall method indent-point state))))))))
+
+;; (setq lisp-indent-function '$lisp-indent-function)
+
 (section "requires"
   (require 'seq)
   (require 's)
@@ -22,37 +75,44 @@
       '("gist.github.com" "dpaste.com" "paste.mozilla.org" "dpaste.org"
         "ix.io" "paste.pound-python.org"))
 
-;; ;; Bind <tab> to what TAB was bound to, in all keymaps that had
-;; ;; TAB but not <tab>. This might miss prefix maps? And "S-<tab>"?
-;; (mapatoms (lambda (sym) (if (and (boundp sym)
-;;                                  (keymapp (symbol-value sym))
-;;                                  (s-ends-with-p "-map" (symbol-name sym)))
-;;                             (let* ((km (symbol-value sym))
-;;                                    (ci-val (lookup-key km (kbd "TAB")))
-;;                                    (tab-val (lookup-key km (kbd "<tab>"))))
-;;                               (if (and ci-val (not tab-val))
-;;                                   (define-key km (kbd "<tab>") ci-val)))))
-;;           obarray)
+         ;; ;; Bind <tab> to what TAB was bound to, in all keymaps that had
+         ;; ;; TAB but not <tab>. This might miss prefix maps? And "S-<tab>"?
+         ;; (mapatoms (lambda (sym) (if (and (boundp sym)
+         ;;                                  (keymapp (symbol-value sym))
+         ;;                                  (s-ends-with-p "-map" (symbol-name sym)))
+         ;;                             (let* ((km (symbol-value sym))
+         ;;                                    (ci-val (lookup-key km (kbd "TAB")))
+         ;;                                    (tab-val (lookup-key km (kbd "<tab>"))))
+         ;;                               (if (and ci-val (not tab-val))
+         ;;                                   (define-key km (kbd "<tab>") ci-val)))))
+         ;;           obarray)
 
-;; (defmacro oklet (bindings &rest body)
-;;   "Let like Clojure's, i.e. without the extra parens.
-;; Can destructure and is sequential.
+(defun oklet-indent-function (pos state)
+  (message "called indent fn with pos %s" pos)
+  (-let* (((depth) state)
+          (open-parens (nth 9 state)))
+    (if (and (= 2 depth)
+             (> (scan-sexps (1+ (car open-parens)) 2) pos))
+        (cadr open-parens)
+      (lisp-indent-function pos state))))
 
-;; Examlpe: (oklet (a 1 (b c) '(2 3)) ...) binds a b and c to 1 2 and 3
-;; respectively."
-;;   (declare (indent common-lisp-indent-function))
-;;   `(-let* ,(seq-partition bindings 2)
-;;      ,@body))
+(defmacro oklet (bindings &rest body)
+  (declare (indent oklet-indent-function))
+  `(-let* ,(seq-partition bindings 2)
+     ,@body))
 
-;; ;; (function-put 'oklet 'common-lisp-indent-function '((&whole 3 0 0 3 0 3 0 (&whole 0 1 2 3) 0) 1 2 3 4 5))
-;; (function-put 'oklet 'common-lisp-indent-function
-;;               '((&whole 6 &rest 0) &rest 0))
+(function-put 'oklet 'common-lisp-indent-function 1)
 
-;; (oklet
-;;       (a 1
-;;          b 2)
-;; (some-call)
-;;       (some-other-call))
+(oklet (aaaaaa 1
+               b 2)
+  (1+ 1)
+(1+ 2))
+
+  "Let like Clojure's, i.e. without the extra parens.
+Can destructure and is sequential.
+
+Examlpe: (oklet (a 1 (b c) '(2 3)) ...) binds a b and c to 1 2 and 3
+respectively."
 
 (defun ok-bind-tab-to-TAB (keymap unbind)
   "Bind <tab> (regular tab) to what TAB (C-i) was bound to in KEYMAP,
