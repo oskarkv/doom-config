e4af1c9f15025895e0ced7302441e3c60af3d9b1
Oskar Kvist <oskar.kvist@gmail.com>
2020-03-29 21:43:57 +0200

    Clean up and add patches.el

--- config.el
+++ config.el
@@ -1,118 +1,19 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+(require 'patches)
 (require 'esexp)
 (require 'utils)
 
-;; (defun $lisp-indent-function (indent-point state)
-;;   "Override `lisp-indent-function' to properly handle plists. See the original function fo full description"
-;;   (let ((normal-indent (current-column))
-;;         (orig-point (point)))
-;;     (goto-char (1+ (elt state 1)))
-;;     (parse-partial-sexp (point) calculate-lisp-indent-last-sexp 0 t)
-;;     (cond
-;;      ;; car of form doesn't seem to be a symbol, or is a keyword
-;;      ((and (elt state 2)
-;;            (or (not (looking-at "\\sw\\|\\s_"))
-;;                (looking-at ":")))
-;;       (if (not (> (save-excursion (forward-line 1) (point))
-;;                   calculate-lisp-indent-last-sexp))
-;;           (progn (goto-char calculate-lisp-indent-last-sexp)
-;;                  (beginning-of-line)
-;;                  (parse-partial-sexp (point)
-;;                                      calculate-lisp-indent-last-sexp 0 t)))
-;;       ;; Indent under the list or under the first sexp on the same
-;;       ;; line as calculate-lisp-indent-last-sexp.  Note that first
-;;       ;; thing on that line has to be complete sexp since we are
-;;       ;; inside the innermost containing sexp.
-;;       (backward-prefix-chars)
-;;       (current-column))
-;;      ((and (save-excursion
-;;              (goto-char indent-point)
-;;              (skip-syntax-forward " ")
-;;              (not (looking-at ":")))
-;;            (save-excursion
-;;              (goto-char orig-point)
-;;              (looking-at ":")))
-;;       (save-excursion
-;;         (goto-char (+ 2 (elt state 1)))
-;;         (current-column)))
-;;      (t
-;;       (let ((function (buffer-substring (point)
-;;                                         (progn (forward-sexp 1) (point))))
-;;             method)
-;;         (setq method (or (function-get (intern-soft function)
-;;                                        'lisp-indent-function)
-;;                          (get (intern-soft function) 'lisp-indent-hook)))
-;;         (cond ((or (eq method 'defun)
-;;                    (and (null method)
-;;                         (> (length function) 3)
-;;                         (string-match "\\`def" function)))
-;;                (lisp-indent-defform state indent-point))
-;;               ((integerp method)
-;;                (lisp-indent-specform method state
-;;                                      indent-point normal-indent))
-;;               (method
-;;                (funcall method indent-point state))))))))
-
-;; (setq lisp-indent-function '$lisp-indent-function)
-
-(section "requires"
-  (require 'seq)
-  (require 's)
-  (require 'cl-indent)
-  (require 'dash))
-
-(setq rcirc-server-alist
-      '(("irc.freenode.net"
-         :channels ("#rcirc" "#emacs" "#evil-mode")))
-      rcirc-default-nick "tufflax"
-      rcirc-authinfo
-      (list (seq-concatenate
-             'list
-             '("freenode" nickserv "tufflax")
-             (list (ok-get-string-from-file "~/.doom.d/.freenode-pass")))))
-
-(setq webpaste-provider-priority
-      '("gist.github.com" "dpaste.com" "paste.mozilla.org" "dpaste.org"
-        "ix.io" "paste.pound-python.org"))
-
-         ;; ;; Bind <tab> to what TAB was bound to, in all keymaps that had
-         ;; ;; TAB but not <tab>. This might miss prefix maps? And "S-<tab>"?
-         ;; (mapatoms (lambda (sym) (if (and (boundp sym)
-         ;;                                  (keymapp (symbol-value sym))
-         ;;                                  (s-ends-with-p "-map" (symbol-name sym)))
-         ;;                             (let* ((km (symbol-value sym))
-         ;;                                    (ci-val (lookup-key km (kbd "TAB")))
-         ;;                                    (tab-val (lookup-key km (kbd "<tab>"))))
-         ;;                               (if (and ci-val (not tab-val))
-         ;;                                   (define-key km (kbd "<tab>") ci-val)))))
-         ;;           obarray)
-
-(defun oklet-indent-function (pos state)
-  (message "called indent fn with pos %s" pos)
-  (-let* (((depth) state)
-          (open-parens (nth 9 state)))
-    (if (and (= 2 depth)
-             (> (scan-sexps (1+ (car open-parens)) 2) pos))
-        (cadr open-parens)
-      (lisp-indent-function pos state))))
-
-(defmacro oklet (bindings &rest body)
-  (declare (indent oklet-indent-function))
-  `(-let* ,(seq-partition bindings 2)
-     ,@body))
-
-(function-put 'oklet 'common-lisp-indent-function 1)
-
-(oklet (aaaaaa 1
-               b 2)
-  (1+ 1)
-(1+ 2))
-
-  "Let like Clojure's, i.e. without the extra parens.
-Can destructure and is sequential.
-
-Examlpe: (oklet (a 1 (b c) '(2 3)) ...) binds a b and c to 1 2 and 3
-respectively."
+;; ;; Bind <tab> to what TAB was bound to, in all keymaps that had
+;; ;; TAB but not <tab>. This might miss prefix maps? And "S-<tab>"?
+;; (mapatoms (lambda (sym) (if (and (boundp sym)
+;;                                  (keymapp (symbol-value sym))
+;;                                  (s-ends-with-p "-map" (symbol-name sym)))
+;;                             (let* ((km (symbol-value sym))
+;;                                    (ci-val (lookup-key km (kbd "TAB")))
+;;                                    (tab-val (lookup-key km (kbd "<tab>"))))
+;;                               (if (and ci-val (not tab-val))
+;;                                   (define-key km (kbd "<tab>") ci-val)))))
+;;           obarray)
 
 (defun ok-bind-tab-to-TAB (keymap unbind)
   "Bind <tab> (regular tab) to what TAB (C-i) was bound to in KEYMAP,
@@ -136,10 +37,6 @@ unless <tab> was already bound."
 (ok-fix-tab
   (org t)
   evil-org)
-
-;; (section "Bind <tab> to what TAB was in some keymaps"
-;;   (ok-fix-tab org)
-;;   (ok-fix-tab evil-org))
 
 ;; Name and email address
 (setq user-full-name "Oskar Kvist"
@@ -196,25 +93,6 @@ unless <tab> was already bound."
 
 (dolist (fn '(switch-to-next-buffer switch-to-prev-buffer))
   (advice-remove fn #'doom-run-switch-to-next-prev-buffer-hooks-a))
-
-;; Redefine evil-delete-buffer to not also close the window
-(evil-define-command evil-delete-buffer (buffer &optional bang)
-  "Deletes a buffer."
-  (interactive "<b><!>")
-  (with-current-buffer (or buffer (current-buffer))
-    (when bang
-      (set-buffer-modified-p nil)
-      (dolist (process (process-list))
-        (when (eq (process-buffer process) (current-buffer))
-          (set-process-query-on-exit-flag process nil))))
-    ;; if the buffer which was initiated by emacsclient,
-    ;; call `server-edit' from server.el to avoid
-    ;; "Buffer still has clients" message
-    (if (and (fboundp 'server-edit)
-             (boundp 'server-buffer-clients)
-             server-buffer-clients)
-        (server-edit)
-      (kill-buffer nil))))
 
 ;;; Utils
 
@@ -299,26 +177,6 @@ unless <tab> was already bound."
   (use-package orgtbl-aggregate
     :ensure t)
 
-  (use-package ok
-    :load-path "~/.emacs.d/lisp/ok/")
-
-  (use-package org
-    ;; only this from archive
-    :pin "org"
-    :init
-    (setq-default
-     org-startup-indented t
-     org-todo-keywords '((sequence "todo" "in-progress" "done")))
-    :ensure t
-    :config
-    (progn
-      (add-hook 'org-src-mode-hook 'evil-normalize-keymaps)
-      (add-hook 'org-mode-hook
-                (lambda () (setq fill-column 70)))))
-
-  (use-package webpaste
-    :ensure t)
-
   (use-package magit
     :ensure t
     :config
@@ -326,9 +184,6 @@ unless <tab> was already bound."
               (lambda ()
                 (setq fill-column 72)
                 (turn-on-auto-fill))))
-
-  (load "~/.emacs.d/elpa/lv.el")
-
 
   (use-package cider
     :load-path "~/code/cider"
@@ -352,6 +207,28 @@ unless <tab> was already bound."
     (add-hook 'hy-mode-hook 'fci-mode)))
 
 ;;; Settings
+
+(setq which-key-show-operator-state-maps t)
+
+(section "requires"
+  (require 'seq)
+  (require 's)
+  (require 'cl-indent)
+  (require 'dash))
+
+(setq rcirc-server-alist
+      '(("irc.freenode.net"
+         :channels ("#rcirc" "#emacs" "#evil-mode")))
+      rcirc-default-nick "tufflax"
+      rcirc-authinfo
+      (list (seq-concatenate
+             'list
+             '("freenode" nickserv "tufflax")
+             (list (ok-get-string-from-file "~/.doom.d/.freenode-pass")))))
+
+(setq webpaste-provider-priority
+      '("gist.github.com" "dpaste.com" "paste.mozilla.org" "dpaste.org"
+        "ix.io" "paste.pound-python.org"))
 
 ;; Avoid double \\ in re-builder
 (setq reb-re-syntax 'string)
---
+++ ok-patches.el
@@ -0,0 +1,368 @@
+;;; ~/.doom.d/patches.el -*- lexical-binding: t; -*-
+
+;; CHANGE: Redefine evil-delete-buffer to not also close the window
+(evil-define-command evil-delete-buffer (buffer &optional bang)
+  "Deletes a buffer."
+  (interactive "<b><!>")
+  (with-current-buffer (or buffer (current-buffer))
+    (when bang
+      (set-buffer-modified-p nil)
+      (dolist (process (process-list))
+        (when (eq (process-buffer process) (current-buffer))
+          (set-process-query-on-exit-flag process nil))))
+    ;; if the buffer which was initiated by emacsclient,
+    ;; call `server-edit' from server.el to avoid
+    ;; "Buffer still has clients" message
+    (if (and (fboundp 'server-edit)
+             (boundp 'server-buffer-clients)
+             server-buffer-clients)
+        (server-edit)
+      (kill-buffer nil))))
+
+(after! clojure-mode
+  ;; CHANGE: Indent docstrings relative to their parent forms
+  (defun clojure-indent-line ()
+    "Indent current line as Clojure code."
+    (if (clojure-in-docstring-p)
+        (save-excursion
+          (let ((indent (save-excursion
+                          (progn (search-backward "\"") (1+ (current-column))))))
+            (beginning-of-line)
+            (looking-at "^\\s-*")
+            (replace-match (make-string indent ? )))))
+    (lisp-indent-line))
+
+  ;; CHANGE: Lots of changes to font locking
+  (setq clojure-font-lock-keywords
+        (eval-when-compile
+          `( ;; Top-level variable definition
+            ;; (,(concat "(\\(?:" clojure--sym-regexp "/\\)?\\("
+            (,(concat "(\\(\\(?:" clojure--sym-regexp "/\\)?"
+                      (regexp-opt '("def" "def-" "defconst" "defonce" "defs"))
+                      ;; variable declarations
+                      "\\)\\>"
+                      ;; Any whitespace
+                      "[ \r\n\t]*"
+                      ;; Possibly type or metadata
+                      "\\(?:#?^\\(?:{[^}]*}\\|\\sw+\\)[ \r\n\t]*\\)*"
+                      "\\(\\sw+\\)?")
+             (1 font-lock-keyword-face)
+             (2 font-lock-variable-name-face nil t))
+            ;; Type definition
+            (,(concat "(\\(?:clojure.core/\\)?\\("
+                      (regexp-opt '("defstruct" "deftype" "defprotocol"
+                                    "defrecord" "deftype-"))
+                      ;; type declarations
+                      "\\)\\>"
+                      ;; Any whitespace
+                      "[ \r\n\t]*"
+                      ;; Possibly type or metadata
+                      "\\(?:#?^\\(?:{[^}]*}\\|\\sw+\\)[ \r\n\t]*\\)*"
+                      "\\(\\sw+\\)?")
+             (1 font-lock-keyword-face)
+             (2 font-lock-type-face nil t))
+            ;; Function definition (anything that starts with def and is not
+            ;; listed above)
+            (,(concat "(\\(\\(?:" clojure--sym-regexp "/\\)?"
+                      "def[^ \r\n\t]*\\)"
+                      ;; Function declarations
+                      "\\>"
+                      ;; Any whitespace
+                      "[ \r\n\t]*"
+                      ;; Possibly type or metadata
+                      "\\(?:#?^\\(?:{[^}]*}\\|\\sw+\\)[ \r\n\t]*\\)*"
+                      (concat "\\(" clojure--sym-regexp "\\)?"))
+             (1 font-lock-keyword-face)
+             (2 font-lock-function-name-face nil t))
+            ;; (fn name? args ...)
+            (,(concat "(\\(?:clojure.core/\\)?\\(fn\\)[ \t]+"
+                      ;; Possibly type
+                      "\\(?:#?^\\sw+[ \t]*\\)?"
+                      ;; Possibly name
+                      "\\(\\sw+\\)?" )
+             (1 font-lock-keyword-face)
+             (2 font-lock-function-name-face nil t))
+            ;; lambda arguments - %, %&, %1, %2, etc
+            ("\\<%[&1-9]?" (0 font-lock-variable-name-face))
+            ;; Special forms
+            (,(concat
+               "("
+               (regexp-opt
+                '("def" "do" "if" "let" "let*" "var" "fn" "fn*" "loop" "loop*"
+                  "recur" "throw" "try" "catch" "finally"
+                  "set!" "new" "."
+                  "monitor-enter" "monitor-exit" "quote") t)
+               "\\>")
+             1 font-lock-keyword-face)
+            ;; Built-in binding and flow of control forms
+            (,(concat
+               "(\\(?:clojure.core/\\)?"
+               (regexp-opt
+                '("letfn" "case" "cond" "cond->" "cond->>" "condp"
+                  "for" "when" "when-not" "when-let" "when-first" "when-some"
+                  "if-let" "if-not" "if-some"
+                  ".." "->" "->>" "as->" "doto" "and" "or"
+                  "dosync" "doseq" "dotimes" "dorun" "doall"
+                  "ns" "in-ns"
+                  "with-open" "with-local-vars" "binding"
+                  "with-redefs" "with-redefs-fn"
+                  "declare") t)
+               "\\>")
+             1 font-lock-keyword-face)
+            ;; Macros similar to let, when, and while
+            ;; (,(rx symbol-start
+            ;;       (or "let" "when" "while") "-"
+            ;;       (1+ (or (syntax word) (syntax symbol)))
+            ;;       symbol-end)
+            ;;  0 font-lock-keyword-face)
+            (,(concat
+               "\\<"
+               (regexp-opt
+                '("*1" "*2" "*3" "*agent*"
+                  "*allow-unresolved-vars*" "*assert*" "*clojure-version*"
+                  "*command-line-args*" "*compile-files*"
+                  "*compile-path*" "*data-readers*" "*default-data-reader-fn*"
+                  "*e" "*err*" "*file*" "*flush-on-newline*"
+                  "*in*" "*macro-meta*" "*math-context*" "*ns*" "*out*"
+                  "*print-dup*" "*print-length*" "*print-level*"
+                  "*print-meta*" "*print-readably*"
+                  "*read-eval*" "*source-path*"
+                  "*unchecked-math*"
+                  "*use-context-classloader*" "*warn-on-reflection*")
+                t)
+               "\\>")
+             0 font-lock-builtin-face)
+            ;; Dynamic variables - *something* or @*something*
+            (,(concat "\\(?:\\<\\|/\\)@?\\(\\*" clojure--sym-regexp "\\*\\)\\>")
+             1 font-lock-variable-name-face)
+            ;; Global constants - nil, true, false
+            (,(concat
+               "\\<"
+               (regexp-opt
+                '("true" "false" "nil") t)
+               "\\>")
+             0 'clojure-number-face)
+            ;; Character literals - \1, \a, \newline, \u0000
+            ("\\\\\\([[:punct:]]\\|[a-z0-9]+\\>\\)" 0 'clojure-character-face)
+
+            ;; namespace definitions: (ns foo.bar)
+            (,(concat "(\\<ns\\>[ \r\n\t]*"
+                      ;; Possibly metadata, shorthand and/or longhand
+                      "\\(?:\\^?\\(?:{[^}]+}\\|:[^ \r\n\t]+[ \r\n\t]\\)[ \r\n\t]*\\)*"
+                      ;; namespace
+                      "\\(" clojure--sym-regexp "\\)")
+             (1 font-lock-type-face))
+
+            ;; TODO dedupe the code for matching of keywords, type-hints and unmatched symbols
+
+            ;; keywords: {:oneword/ve/yCom|pLex.stu-ff 0}
+            (,(concat "\\(:\\{1,2\\}\\)\\(" clojure--sym-regexp "?\\)\\(/\\)\\(" clojure--sym-regexp "\\)")
+             (1 'clojure-keyword-face)
+             (2 font-lock-type-face)
+             ;; (2 'clojure-keyword-face)
+             (3 'default)
+             (4 'clojure-keyword-face))
+            (,(concat "\\(:\\{1,2\\}\\)\\(" clojure--sym-regexp "\\)")
+             (1 'clojure-keyword-face)
+             (2 'clojure-keyword-face))
+
+            ;; type-hints: #^oneword
+            (,(concat "\\(#?\\^\\)\\(" clojure--sym-regexp "?\\)\\(/\\)\\(" clojure--sym-regexp "\\)")
+             (1 'default)
+             (2 font-lock-type-face)
+             (3 'default)
+             (4 'default))
+            (,(concat "\\(#?\\^\\)\\(" clojure--sym-regexp "\\)")
+             (1 'default)
+             (2 font-lock-type-face))
+
+            ;; clojure symbols not matched by the previous regexps; influences CIDER's
+            ;; dynamic syntax highlighting (CDSH). See https://git.io/vxEEA:
+            ;; (,(concat "\\(" clojure--sym-regexp "?\\)\\(/\\)\\(" clojure--sym-regexp "\\)")
+            ;;  (1 font-lock-type-face)
+            ;;  ;; 2nd and 3th matching groups can be font-locked to `nil' or `default'.
+            ;;  ;; CDSH seems to kick in only for functions and variables referenced w/o
+            ;;  ;; writing their namespaces.
+            ;;  (2 nil)
+            ;;  (3 clojure-pink-face))
+            ;; (,(concat "\\(" clojure--sym-regexp "\\)")
+            ;;  ;; this matching group must be font-locked to `nil' otherwise CDSH breaks.
+            ;;  (1 nil))
+
+            ;; #_ and (comment ...) macros.
+            (clojure--search-comment-macro 1 font-lock-comment-face t)
+            ;; Highlight `code` marks, just like `elisp'.
+            (,(rx "`" (group-n 1 (optional "#'")
+                               (+ (or (syntax symbol) (syntax word)))) "`")
+             (1 'font-lock-constant-face prepend))
+            ;; Highlight [[var]] comments
+            (,(rx "[[" (group-n 1 (optional "#'")
+                                (+ (or (syntax symbol) (syntax word)))) "]]")
+             (1 'font-lock-constant-face prepend))
+            ;; Highlight escaped characters in strings.
+            (clojure-font-lock-escaped-chars 0 'bold prepend)
+            ;; Highlight grouping constructs in regular expressions
+            (clojure-font-lock-regexp-groups
+             (1 'font-lock-regexp-grouping-construct prepend))
+            ;; ("\\<-?[0-9]+\\(\\.[0-9]+\\)?\\>" 0 'clojure-number-face)
+            (,(concat "\\<" clojure--num-regexp "\\(\\(e\\|E\\)" clojure--num-regexp "\\)?\\>")
+             0 'clojure-number-face)
+            ("[~@'`#]" 0 'clojure-quote-face)))
+        "Default expressions to highlight in Clojure mode."))
+
+(after! cider
+  ;; CHANGE: Fix (def v (a-fn ...)) not font locking a-fn
+  (defun cider--parse-and-apply-locals (end &optional outer-locals)
+    "Figure out local variables between point and END.
+A list of these variables is set as the `cider-locals' text property over
+the code where they are in scope.
+Optional argument OUTER-LOCALS is used to specify local variables defined
+before point."
+    (while (search-forward-regexp "(\\(ns\\_>\\|def\\|fn\\|for\\b\\|loop\\b\\|with-\\|do[a-z]+\\|\\([a-z]+-\\)?let\\b\\)"
+                                  end 'noerror)
+      (goto-char (match-beginning 0))
+      (let ((sym (match-string 1))
+            (beg (match-beginning 0))
+            whole-sym
+            (sexp-end (save-excursion
+                        (or (ignore-errors (forward-sexp 1)
+                                           (point))
+                            end))))
+        ;; #1324: Don't do dynamic font-lock in `ns' forms, they are special
+        ;; macros where nothing is evaluated, so we'd get a lot of false
+        ;; positives.
+        (if (equal sym "ns")
+            (add-text-properties (point) sexp-end '(cider-block-dynamic-font-lock t))
+          (forward-char 1)
+          (forward-sexp 1)
+          (setq whole-sym (buffer-substring-no-properties (1+ beg) (point)))
+          (let ((locals (append outer-locals
+                                (if (or (string= whole-sym "def")
+                                        (string= whole-sym "defconst"))
+                                    '()
+                                  (pcase sym
+                                    ((or "fn" "def" "") (cider--read-locals-from-arglist))
+                                    (_ (cider--read-locals-from-bindings-vector)))))))
+            (add-text-properties (point) sexp-end (list 'cider-locals locals))
+            (clojure-forward-logical-sexp 1)
+            (cider--parse-and-apply-locals sexp-end locals)))
+        (goto-char sexp-end))))
+
+  ;; CHANGE: Fix docstring lines starting with spaces showing in docview
+  (defun cider-docview-render-info (buffer info)
+    "Emit into BUFFER formatted INFO for the Clojure or Java symbol."
+    (let* ((ns      (nrepl-dict-get info "ns"))
+           (name    (nrepl-dict-get info "name"))
+           (added   (nrepl-dict-get info "added"))
+           (depr    (nrepl-dict-get info "deprecated"))
+           (macro   (nrepl-dict-get info "macro"))
+           (special (nrepl-dict-get info "special-form"))
+           (forms   (when-let* ((str (nrepl-dict-get info "forms-str")))
+                      (split-string str "\n")))
+           (args    (when-let* ((str (nrepl-dict-get info "arglists-str")))
+                      (split-string str "\n")))
+           (doc     (replace-regexp-in-string
+                     "^ *" "  "
+                     (or (nrepl-dict-get info "doc")
+                         "Not documented.")))
+           (url     (nrepl-dict-get info "url"))
+           (class   (nrepl-dict-get info "class"))
+           (member  (nrepl-dict-get info "member"))
+           (javadoc (nrepl-dict-get info "javadoc"))
+           (super   (nrepl-dict-get info "super"))
+           (ifaces  (nrepl-dict-get info "interfaces"))
+           (spec    (nrepl-dict-get info "spec"))
+           (clj-name  (if ns (concat ns "/" name) name))
+           (java-name (if member (concat class "/" member) class))
+           (see-also (nrepl-dict-get info "see-also")))
+      (cider--help-setup-xref (list #'cider-doc-lookup (format "%s/%s" ns name)) nil buffer)
+      (with-current-buffer buffer
+        (cl-flet ((emit (text &optional face)
+                        (insert (if face
+                                    (propertize text 'font-lock-face face)
+                                  text)
+                                "\n")))
+          (emit (if class java-name clj-name) 'font-lock-function-name-face)
+          (when super
+            (emit (concat "   Extends: " (cider-font-lock-as 'java-mode super))))
+          (when ifaces
+            (emit (concat "Implements: " (cider-font-lock-as 'java-mode (car ifaces))))
+            (dolist (iface (cdr ifaces))
+              (emit (concat "            "(cider-font-lock-as 'java-mode iface)))))
+          (when (or super ifaces)
+            (insert "\n"))
+          (when-let* ((forms (or forms args)))
+            (dolist (form forms)
+              (insert " ")
+              (emit (cider-font-lock-as-clojure form))))
+          (when special
+            (emit "Special Form" 'font-lock-keyword-face))
+          (when macro
+            (emit "Macro" 'font-lock-variable-name-face))
+          (when added
+            (emit (concat "Added in " added) 'font-lock-comment-face))
+          (when depr
+            (emit (concat "Deprecated in " depr) 'font-lock-keyword-face))
+          (if class
+              (cider-docview-render-java-doc (current-buffer) doc)
+            (emit doc))
+          (when url
+            (insert "\n  Please see ")
+            (insert-text-button url
+                                'url url
+                                'follow-link t
+                                'action (lambda (x)
+                                          (browse-url (button-get x 'url))))
+            (insert "\n"))
+          (when javadoc
+            (insert "\n\nFor additional documentation, see the ")
+            (insert-text-button "Javadoc"
+                                'url javadoc
+                                'follow-link t
+                                'action (lambda (x)
+                                          (browse-url (button-get x 'url))))
+            (insert ".\n"))
+          (insert "\n")
+          (when spec
+            (emit "Spec:" 'font-lock-function-name-face)
+            (insert (cider-browse-spec--pprint-indented spec))
+            (insert "\n\n")
+            (insert-text-button "Browse spec"
+                                'follow-link t
+                                'action (lambda (_)
+                                          (cider-browse-spec (format "%s/%s" ns name))))
+            (insert "\n\n"))
+          (if cider-docview-file
+              (progn
+                (insert (propertize (if class java-name clj-name)
+                                    'font-lock-face 'font-lock-function-name-face)
+                        " is defined in ")
+                (insert-text-button (cider--abbreviate-file-protocol cider-docview-file)
+                                    'follow-link t
+                                    'action (lambda (_x)
+                                              (cider-docview-source)))
+                (insert "."))
+            (insert "Definition location unavailable."))
+          (when see-also
+            (insert "\n\n Also see: ")
+            (mapc (lambda (ns-sym)
+                    (let* ((ns-sym-split (split-string ns-sym "/"))
+                           (see-also-ns (car ns-sym-split))
+                           (see-also-sym (cadr ns-sym-split))
+                           ;; if the var belongs to the same namespace,
+                           ;; we omit the namespace to save some screen space
+                           (symbol (if (equal ns see-also-ns) see-also-sym ns-sym)))
+                      (insert-text-button symbol
+                                          'type 'help-xref
+                                          'help-function (apply-partially #'cider-doc-lookup symbol)))
+                    (insert " "))
+                  see-also))
+          (cider--doc-make-xrefs)
+          (let ((beg (point-min))
+                (end (point-max)))
+            (nrepl-dict-map (lambda (k v)
+                              (put-text-property beg end k v))
+                            info)))
+        (current-buffer)))))
+
+(provide 'patches)
