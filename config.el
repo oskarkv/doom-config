cfc3e22241c44903a10792786f7c2e3624167e71
Oskar Kvist <oskar.kvist@gmail.com>
2020-10-12 20:59:43 +0200

    Bind key to +format/buffer in Python

--- config.el
+++ config.el
@@ -604,6 +604,7 @@ BUF should be skipped over by functions like `next-buffer' and `other-buffer'."
       :n "M-I" (cmd (ok-python-transpose-big-thing 1 t))
       :n "M-N" (cmd (ok-python-transpose-big-thing -1 t))
       :prefix "SPC"
+      :n "cf" '+format/buffer
       :n "w" (cmd (evil-execute-macro 1 "ysll)l"))
       :n "jj" 'run-python
       :n "eb" 'python-shell-send-buffer
