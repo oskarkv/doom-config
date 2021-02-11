f7323f4cec07e28da194c4f6a888cf1549d22323
Oskar Kvist <oskar.kvist@gmail.com>
2021-02-11 13:50:04 +0100

    Move default keybindings up in config

--- config.el
+++ config.el
@@ -693,6 +693,105 @@ BUF should be skipped over by functions like `next-buffer' and `other-buffer'."
       (backward-char)
       (insert "`"))))
 
+(map! :e "C-," 'evil-exit-emacs-state
+      :o "l" -operator-inside-keymap
+      :o "i" nil
+      :o "o" (cmd (forward-evil-symbol))
+      :o "e" 'evil-next-line
+      :o "u" 'evil-previous-line
+      ;; Makes tab not call yas-expand
+      :ir "ESC" 'doom/escape
+      :ir "<escape>" 'doom/escape
+      :i [tab] 'complete-symbol
+      :i "§" 'evil-normal-state
+      :v "§" 'evil-exit-visual-state
+      :i "C-e" 'doom/delete-backward-word
+      :i "C-<tab>" (cmd (insert "\t"))
+      :r "§" 'evil-normal-state
+      :nmvi "TAB" nil
+      "C-i" 'end-of-line
+      "C-n" 'beginning-of-line
+      "C-s" 'save-buffer
+      "C-ä" 'other-window
+      "C-å" 'ace-window
+      "M-c" 'evil-snipe-repeat-reverse
+      "M-v" 'evil-snipe-repeat
+      "M-f" 'switch-to-prev-buffer
+      "M-p" 'switch-to-next-buffer
+      "M-w" 'er/expand-region
+      :nv "gB" 'ok-remove-some-spaces
+      :nv "gb" 'ok-remove-spaces
+      :nv "gh" 'ok-evil-webpaste
+      :nv "go" '+evil:yank-unindented
+      :m "gs" nil
+      :nv "gs" 'ok-evil-three-backticks-yank
+      :nv "gy" 'ok-evil-reddit-yank
+      :n "§" (cmd (evil-ex-nohighlight) (evil-force-normal-state))
+      :n "C-n" nil
+      :n "u" nil
+      :n "e" nil
+      :n "j" nil
+      :n "J" nil
+      :n "M" nil
+      :n "n" nil
+      :n "N" nil
+      :n "h" nil
+      :n "H" nil
+      :n "i" nil
+      :n "I" nil
+      :n "SPC" nil
+      :n "zx" 'evil-delete-buffer
+      :n "k" 'undo
+      :n "E" 'ok-evil-join
+      :n "Å" 'newline-and-indent
+      :n "l" 'evil-insert
+      :n "L" 'evil-insert-line
+      :n "C-x" 'evil-numbers/inc-at-pt
+      :n "C-z" 'evil-numbers/dec-at-pt
+      :n "M-t" #'ok-run-q-macro
+      :m "SPC" nil
+      :m "k" nil
+      :m "E" nil
+      :m "l" nil
+      :m "L" nil
+      :m "u" 'evil-previous-visual-line
+      :m "e" 'evil-next-visual-line
+      :m "C-u" 'ok-move-up-15-lines
+      :m "C-e" 'ok-move-down-15-lines
+      :m "j" 'evil-forward-word-end
+      :m "J" 'evil-forward-WORD-end
+      :m "n" 'evil-backward-char
+      :m "h" 'evil-ex-search-next
+      :m "H" 'evil-ex-search-previous
+      :m "i" 'evil-forward-char
+      :m "å" 'ace-window
+      :m "ä" 'evil-window-next
+      :m "Ä" 'evil-window-prev
+      :m "C-q" 'evil-visual-block
+      :m "<C-i>" nil
+      ;; :m "C-y" 'evil-jump-backward
+      ;; :m "C-o" 'evil-jump-forward
+      :m "C-y" 'better-jumper-jump-backward
+      :m "C-o" 'better-jumper-jump-forward
+      :n "M-y" 'goto-last-change
+      :n "M-o" 'goto-last-change-reverse
+      :m "C-," 'evil-emacs-state
+      :v "u" nil
+      :v "l" -visual-inside-keymap
+      :v "i" nil
+      :v "L" 'evil-insert
+      :v "A" 'evil-append
+      :prefix "SPC"
+      :nv "gl" 'magit-log-buffer-file
+      :n "gb" 'magit-blame-addition
+      :n "ta" (cmd (shell-command "jobbsetup"))
+      :n "tf" (cmd (shell-command "jobbsetup pytest-failing"))
+      :n "tl" (cmd (shell-command "jobbsetup only linter"))
+      :n "tm" (cmd (shell-command "jobbsetup only mypy"))
+      :n "ts" (cmd (shell-command "jobbsetup only tests"))
+      :n "q" 'ok-wrap-word-in-backticks
+      :nv "f" 'fill-paragraph)
+
 (map! :after company
       :map company-active-map
       "C-e" 'company-select-next
@@ -962,100 +1061,6 @@ BUF should be skipped over by functions like `next-buffer' and `other-buffer'."
     (goto-char beg)
     (dotimes (_ count)
       (join-line 1))))
-
-(map! :e "C-," 'evil-exit-emacs-state
-      :o "l" -operator-inside-keymap
-      :o "i" nil
-      :o "o" (cmd (forward-evil-symbol))
-      :o "e" 'evil-next-line
-      :o "u" 'evil-previous-line
-      ;; Makes tab not call yas-expand
-      :i [tab] 'complete-symbol
-      :i "§" 'evil-normal-state
-      :v "§" 'evil-exit-visual-state
-      :i "C-e" 'doom/delete-backward-word
-      :i "C-<tab>" (cmd (insert "\t"))
-      :r "§" 'evil-normal-state
-      :nmvi "TAB" nil
-      "C-i" 'end-of-line
-      "C-n" 'beginning-of-line
-      "C-s" 'save-buffer
-      "C-ä" 'other-window
-      "C-å" 'ace-window
-      "M-c" 'evil-snipe-repeat-reverse
-      "M-v" 'evil-snipe-repeat
-      "M-f" 'switch-to-prev-buffer
-      "M-p" 'switch-to-next-buffer
-      "M-w" 'er/expand-region
-      :nv "gB" 'ok-remove-some-spaces
-      :nv "gb" 'ok-remove-spaces
-      :nv "gh" 'ok-evil-webpaste
-      :nv "go" '+evil:yank-unindented
-      :m "gs" nil
-      :nv "gs" 'ok-evil-three-backticks-yank
-      :nv "gy" 'ok-evil-reddit-yank
-      :n "§" (cmd (evil-ex-nohighlight) (evil-force-normal-state))
-      :n "C-n" nil
-      :n "u" nil
-      :n "e" nil
-      :n "j" nil
-      :n "J" nil
-      :n "M" nil
-      :n "n" nil
-      :n "N" nil
-      :n "h" nil
-      :n "H" nil
-      :n "i" nil
-      :n "I" nil
-      :n "SPC" nil
-      :n "zx" 'evil-delete-buffer
-      :n "k" 'undo
-      :n "E" 'ok-evil-join
-      :n "Å" 'newline-and-indent
-      :n "l" 'evil-insert
-      :n "L" 'evil-insert-line
-      :n "M-y" 'goto-last-change
-      :n "M-o" 'goto-last-change-reverse
-      :n "C-x" 'evil-numbers/inc-at-pt
-      :n "C-z" 'evil-numbers/dec-at-pt
-      :n "M-t" #'ok-run-q-macro
-      :m "SPC" nil
-      :m "k" nil
-      :m "E" nil
-      :m "l" nil
-      :m "L" nil
-      :m "u" 'evil-previous-visual-line
-      :m "e" 'evil-next-visual-line
-      :m "C-u" 'ok-move-up-15-lines
-      :m "C-e" 'ok-move-down-15-lines
-      :m "j" 'evil-forward-word-end
-      :m "J" 'evil-forward-WORD-end
-      :m "n" 'evil-backward-char
-      :m "h" 'evil-ex-search-next
-      :m "H" 'evil-ex-search-previous
-      :m "i" 'evil-forward-char
-      :m "å" 'ace-window
-      :m "ä" 'evil-window-next
-      :m "Ä" 'evil-window-prev
-      :m "C-q" 'evil-visual-block
-      :m "C-y" 'evil-jump-backward
-      :m "C-o" 'evil-jump-forward
-      :m "C-," 'evil-emacs-state
-      :v "u" nil
-      :v "l" -visual-inside-keymap
-      :v "i" nil
-      :v "L" 'evil-insert
-      :v "A" 'evil-append
-      :prefix "SPC"
-      :nv "gl" 'magit-log-buffer-file
-      :n "gb" 'magit-blame-addition
-      :n "ta" (cmd (shell-command "jobbsetup"))
-      :n "tf" (cmd (shell-command "jobbsetup pytest-failing"))
-      :n "tl" (cmd (shell-command "jobbsetup only linter"))
-      :n "tm" (cmd (shell-command "jobbsetup only mypy"))
-      :n "ts" (cmd (shell-command "jobbsetup only tests"))
-      :n "q" 'ok-wrap-word-in-backticks
-      :nv "f" 'fill-paragraph)
 
 ;; Apparently C-x is a common prefix, and many modes use it.
 ;; Shadow all of them for now.
